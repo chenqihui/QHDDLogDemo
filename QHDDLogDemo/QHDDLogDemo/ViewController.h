ef65b925f96afb9ee457542b899fd603423a871f
anakinChen <qihuichen@sohu-inc.com>
2017-09-27 20:13:46 +0800

    【New】【项目】初始化

---
+++ QHDDLogDemo/QHDDLogDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QHDDLogDemo
+//
+//  Created by Anakin chen on 2017/9/27.
+//  Copyright © 2017年 AnakinChen Network Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
